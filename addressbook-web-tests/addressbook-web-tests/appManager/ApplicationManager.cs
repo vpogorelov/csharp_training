1e60cabf84d5e590438ef3c640445f830b2cda2e
vpogorelov <v.pgr@yandex.ru>
2017-10-09 08:48:56 +0300

    _public_ ApplicationManager

--- addressbook-web-tests/addressbook-web-tests/appManager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/appManager/ApplicationManager.cs
@@ -9,7 +9,7 @@ using OpenQA.Selenium.Support.UI;
 
 namespace WebAddressbookTests
 {
-    class ApplicationManager
+    public class ApplicationManager
     {
         protected IWebDriver driver;
         protected string baseURL;
