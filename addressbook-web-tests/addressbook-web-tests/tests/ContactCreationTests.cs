6f498ee8d35fedaccdabb164e4e468f724f9cf91
vpogorelov <v.pgr@yandex.ru>
2017-10-25 11:22:09 +0300

    Переустановил VS.   check

--- addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -23,7 +24,23 @@ namespace WebAddressbookTests
             return contacts;
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0], parts[1])
+                {
+                    MiddleName = parts[2]
+                });
+            }
+            return contacts;
+        }
+    
+        [Test, TestCaseSource("ContactDataFromFile")]
         public void ContactCreationTest(ContactData contact)
         {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
